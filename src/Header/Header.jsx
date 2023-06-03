07b2efcd3d5ef6c95ffa6a8e91fd9d8db74dc481
DianaAleksieieva <aleksodiana@gmail.com>
2023-06-02 23:31:54 -0400

    fix

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -4,12 +4,10 @@ import logo from '../img/LOGO-DI.svg';
 function Header() {
   return (
     <header className={styles.headerContainer} >
+      <div className={styles.mainNavigation}>
         <img height={50}  src={logo} alt="logo" />
-        {/* <nav className={styles.mainNavigation}>
-            <p className={styles.mainNavigationItem}>me</p>
-            <p className={styles.mainNavigationItem}>design</p>
-            <p className={styles.mainNavigationItem}>dev</p>
-        </nav> */}
+            <a className={styles.emailLink} href="mailto:aleksodiana@gmail.com">aleksodiana@gmail.com</a>
+            </div>
       </header>
   );
 }
--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -11,16 +11,11 @@ function Hero() {
       <div className={styles.heroDescription} >
         <div className={styles.textDescription}>
           <p className={styles.fullName}> Diana Aleksa </p>
-          <p>
-            Frontend Developer <br></br> Graphic Designer{' '}
+          <p className={styles.myRole}> Graphic Designer
           </p>
           <p className={styles.aboutInfo}>
-            I am creating web pages with the modern JS framework - REACT, using
-            HTML, CSS, and principles of responsive design. Moreover, I am
-            working with API, Redux, and Mongoose. I have skills in graphic
-            programs such as Figma, Photoshop, and Illustrator and may create
-            designs, and icons and change image sizes and formats as well as
-            proficiently retouch photos.
+          I have been working as a designer for 5 years. My main duties were to create a design for printing catalogs, flyers, and envelopes for the cosmetic company Yves Rocher and prepare them for print.
+I also have worked with digital products - emails, banners, and web designs and I am dealing well with retouching. 
           </p>
           <div className={styles.myCVandIn}>
             <a className={styles.downloadButton} href={myCV} download>Download my CV</a>
@@ -36,16 +31,16 @@ function Hero() {
             </a>
           </div>
         </div>
-        <div className={styles.myPhotoContainer}>
+        {/* <div className={styles.myPhotoContainer}> */}
           <img className={styles.myPhoto} src={myPhoto} alt="Diana-main"></img>
-          <p>Would you like to get free design from me? </p>
+          {/* <p>Would you like to get free design from me? </p>
           <a className={styles.freeDesignButton} href="https://forms.gle/mTZsP6Qk7hMcd2Mm8" >Design for FREE</a>
-        </div>
+        </div> */}
       </div>
       <div className={styles.arrowContainer}>
       {/* <img className={styles.portfolioArrow} src={portfolioArrow} alt="decorative arrow"></img> */}
       <div>
-      <p>Design, Development, Prepress, Retouch</p>
+      {/* <p>Design, Prepress, Retouch</p> */}
       <a className={styles.emailLink} href="mailto:aleksodiana@gmail.com">aleksodiana@gmail.com</a>
       </div>
       </div>
